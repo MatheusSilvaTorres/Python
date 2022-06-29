235344771ac0e0f287b5655af4fc82bfeb391912
matheussilvatorres <matheustorres201900@gmail.com>
2022-06-29 20:55:03 -0300

    Create ex001.py

---
+++ exercicio/ex001.py
@@ -0,0 +1,4 @@
+#Implemente um programa que leia um número inteiro e mostre seu sucessor e seu antecessor.
+
+n = int(input('Digite um número: '))
+print('número: {}, antecessor: {}, sucessor: {}'.format(n, n-1, n+1))
