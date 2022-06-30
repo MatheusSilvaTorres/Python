e219f2ee71f34366c07567160ed3cf0fe17a4077
matheussilvatorres <matheustorres201900@gmail.com>
2022-06-30 20:06:18 -0300

    Create ex002.py

---
+++ exercicio/ex002.py
@@ -0,0 +1,6 @@
+# mplemente um programa que leia um número inteiro e mostre seu dobro e seu triplo.
+
+num = int(input('DIgite um número inteiro: '))
+dobro = num * 2
+triplo = num * 3
+print('Número é: {1}, Seu dobro é: {0}, Seu triplo é: {2}'.format(dobro,num,triplo))
