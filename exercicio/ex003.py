643214240b7038515baaab32975ae422f262b746
matheussilvatorres <matheustorres201900@gmail.com>
2022-07-02 14:41:32 -0300

    Update ex003.py

--- exercicio/ex003.py
+++ exercicio/ex003.py
@@ -1,1 +1,6 @@
 # Implemente um programa que leia 4 notas de um aluno e mostre a média aritmética.
+
+n = int(input('Digite um número: '))
+print('número: {}, seu dobro: {}, seu triplo: {}, raiz quadrada: {:.2f}'.format(n, n * 2, n * 3, n ** (1 / 2)))
+# outra forma de calcular raiz quadrada
+print(pow(n, (1/2)))
